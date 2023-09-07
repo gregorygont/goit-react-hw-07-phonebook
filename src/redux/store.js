ccf23bc3fc3b95c8fe7efb3df782911a80cda99b
Grygorii Gontarchuk <105290753+gregorygont@users.noreply.github.com>
2023-09-07 21:57:29 +0300

    fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './sliceContact';
-import { filterReducer } from './sliceFilter';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    phonebook: contactsReducer,
     filter: filterReducer,
   },
 });
